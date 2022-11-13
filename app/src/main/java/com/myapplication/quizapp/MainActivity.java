8c2fcd9787e55c179b8ec6c606af9fb7eb8bac8a
syedaLaiba <laiba033545@gmail.com>
2022-11-13 18:56:29 +0500

    adding basic functionality and layout

--- app/src/main/java/com/myapplication/quizapp/MainActivity.java
+++ app/src/main/java/com/myapplication/quizapp/MainActivity.java
@@ -1,14 +1,117 @@
 package com.myapplication.quizapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView question, totalQuestion,remQues,correctRes,wrongRes;
+    Button btnA, btnB, btnC, btnD, btnSubmit,restartBtn;
+    int correct = 0;
+    int j=0;
+    int wrong_Answer = 0;
+    int totalQuestions = QuestionData.questions.length;
+    int remQuestions=totalQuestions;
+    int currentQuestion = 0, qNo = 0,index=0;
+    String answer = "",selectedAnswer="";
+    ArrayList<Integer>numbers=shuffleQuestionList();
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalQuestion = findViewById(R.id.totalQuestion);
+        question = findViewById(R.id.QuestionText);
+        btnA = findViewById(R.id.AnswerA);
+        btnB = findViewById(R.id.AnswerB);
+        btnC = findViewById(R.id.AnswerC);
+        btnD = findViewById(R.id.AnswerD);
+        btnSubmit = findViewById(R.id.submit);
+        remQues=findViewById(R.id.remainingQues);
+        restartBtn=findViewById(R.id.restart);
+        correctRes=findViewById(R.id.Correct);
+        wrongRes=findViewById(R.id.Wrong);
+        correctRes.setVisibility(View.INVISIBLE);
+        wrongRes.setVisibility(View.INVISIBLE);
+        restartBtn.setVisibility(View.INVISIBLE);
+
+
+        btnA.setOnClickListener(this);
+        restartBtn.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+        btnSubmit.setOnClickListener(this);
+
+        totalQuestion.setText("Total Question: " + totalQuestions);
+        remQues.setText("Remaining: "+remQuestions);
+
+        generateMCQ();
     }
+
+
+    @Override
+    public void onClick(View view) {
+
+
+    }
+    public void generateQuestion(int index)
+    {}
+    private void generateMCQ() {
+        if(remQuestions==0)
+        {
+            showResult();
+            return;
+        }
+        qNo=numbers.get(index);
+        index++;
+        switch (qNo) {
+            case 0:
+                generateQuestion(qNo);
+            case 1:
+                generateQuestion(qNo);
+
+            case 2:
+                generateQuestion(qNo);
+            case 3:
+                generateQuestion(qNo);
+            case 4:
+                generateQuestion(qNo);
+            case 5:
+                generateQuestion(qNo);
+            case 6:
+                generateQuestion(qNo);
+            case 7:
+                generateQuestion(qNo);
+            case 8:
+                generateQuestion(qNo);
+            case 9:
+                generateQuestion(qNo);
+
+
+        }
+    }
+    void showResult()
+    {
+
+    }
+    void restartQuiz(){
+
+    }
+
 }
---
+++ app/src/main/java/com/myapplication/quizapp/QuestionData.java
@@ -0,0 +1,56 @@
+package com.myapplication.quizapp;
+
+
+import java.util.ArrayList;
+
+public class QuestionData {
+    public  static  String[] questions= {
+            "Number of primitive data types in Java are?",
+            "What is the size of float and double in java?",
+            "Find the output of the following code:\n" + "int Integer = 24;\n" + "char String  = ‘I’;\n" + "System.out.print(Integer);\n" + "System.out.print(String);",
+            "Select the valid statement.",
+            "Find the value of A[1] after execution of the following program:\n" +
+                    "int[] A = {0,2,4,1,3};\n" +
+                    "for(int i = 0; i < a.length; i++){\n" +
+                    "    a[i] = a[(a[i] + 3) % a.length];\n" +
+                    "}",
+            "Identify the keyword among the following that makes a variable belong to a class,rather than being defined for each instance of the class.",
+            "In which of the following is toString() method defined?",
+            "What does the following string do to given string str1.\n" + "String str1 = “Interviewbit”.replace(‘e’,’s’);",
+            "What is Runnable?",
+            "Identify the incorrect Java feature."
+    };
+    public  static  String[][] questionsOptions= {
+            {"6"," 7"," 8","9"},
+            {"32 and 64"," 32 and 32"," 64 and 64","64 and 28"},
+            {"Compile Error", "Throw Exception" , "I ", "24 I"},
+            { "char [] ch=new char(5)" , "char [] ch=new char[5]" , "char [] ch=new char()" , "char [] ch=new char[][]" , "char [] ch=new char[5]"},
+            {"0", "1" , "2 " , "3"},
+            {"final" , "static" , "volatile" , "abstract"},
+            {"java.lang.Object", "java.lang.String" , "java.lang.util" , "None"},
+            {"Replaces single occurrence of 'e' to 's'" , "Replaces all occurrences of 'e' to 's'" , "Replaces single occurrence of 's' to 'e'" , "None"},
+            {"Abstract class" , "Interface" , "Class" , "Method"},
+            {"Object Oriented", "Use of Pointers", "Dynamic", "Architectural neural"}
+
+
+    };
+    public static String answers[]={
+            "8",
+            "32 and 64",
+            "24 I",
+            "char [] ch=new char[5]",
+            "1",
+            "static",
+            "java.lang.Object",
+            "Replaces all occurrences of 'e' to 's'",
+            "Interface",
+            "Use of Pointers"
+
+
+
+
+
+    };
+
+
+    }
